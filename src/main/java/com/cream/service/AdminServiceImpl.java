9c1cbf5c814340eff20030c5c3befa5ee37f6138
hoseok <swiftie52@naver.com>
2024-10-11 10:00:08 +0900

    fix : AdminServiceImpl

--- src/main/java/com/cream/service/AdminServiceImpl.java
+++ src/main/java/com/cream/service/AdminServiceImpl.java
@@ -13,27 +13,32 @@ public class AdminServiceImpl implements AdminService {
     AdminDAO adminDAO=new AdminDAOImpl();
 
     @Override
-    public int deleteUserById(int userNo) throws SQLException {
+    public int deleteUserById(int userNo) throws SQLException
+    {
         return adminDAO.deleteUserById(userNo);
     }
 
     @Override
-    public int deleteProductById(int productNo) throws SQLException {
+    public int deleteProductById(int productNo) throws SQLException
+    {
         return adminDAO.deleteProductById(productNo);
     }
 
     @Override
-    public List<SalesDTO> getUnapprovedProducts() throws SQLException {
+    public List<SalesDTO> getUnapprovedProducts() throws SQLException
+    {
         return adminDAO.getUnapprovedProducts();
-    }// 검수 받지 못한 상품 목록들 확인
+    }
 
     @Override
-    public int updateSalesStatus(int salesNo, int salesStatus, char grade) throws SQLException {
+    public int updateSalesStatus(int salesNo, int salesStatus, char grade) throws SQLException
+    {
         return adminDAO.updateSalesStatus(salesNo, salesStatus, grade);
-    }// 업데이트 결과 반환
+    }
 
     @Override
-    public int submitSurvey(SurveyDTO surveyData) throws SQLException {
+    public int submitSurvey(SurveyDTO surveyData) throws SQLException
+    {
         return adminDAO.submitSurvey(surveyData);
-    }// 제출 결과 반환
+    }
 }
